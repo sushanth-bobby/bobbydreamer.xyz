61be19b87d69da3e702cad7e56ac9ab8b02a5c4c
Sushanth Bobby Lloyds <sushanth.august@gmail.com>
2024-10-05 00:29:53 +0530

    Updated config + Got linksHeader and darkmode icon to same level

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "bobby_dreamer",
+    pageTitleSuffix: " | bobby_dreamer",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "en-US", 
+    baseUrl: "bobydreamer.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
