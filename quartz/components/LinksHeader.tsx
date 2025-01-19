44d5ada255a02cd743e84aee7e12162d8d854977
Sushanth Bobby Lloyds <sushanth.gcp@gmail.com>
2025-01-19 16:24:49 +0530

    Updated LinksHeader.tsx for About_me, TIL, iRevere

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -12,7 +12,7 @@ export default (() => {
         <div id="links-header">
           <span>
             {/* <img src="../assets/user-secret-solid.svg" style="color: salmon; background-color: papayawhip; font-size:1.5em;"></img> */}
-            <a href="/about_me">about_me</a>
+            <a href="/pages/about_me">about_me</a>
           </span>
           <span>
             {/* <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Books/Color/books_color.svg"></img> */}
@@ -20,11 +20,11 @@ export default (() => {
           </span>
           <span>
             {/* <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Brain/Color/brain_color.svg"></img> */}
-            <a href="/irevere">iRevere</a>
+            <a href="/pages/irevere">iRevere</a>
           </span>
           <span>
             {/* <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Parrot/Color/parrot_color.svg"></img> */}
-            <a href="/til">T.I.L</a>
+            <a href="/pages/til">T.I.L</a>
           </span>
           <span>
             {/* Quick Commands */}
